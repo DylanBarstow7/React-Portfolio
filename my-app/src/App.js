f7d29fe03bc2df972bbadec32cfbf80d40fc158b
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-07 16:58:59 -0500

    creating contact and skill cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Project from './components/Project';
 import Hero from './components/Hero';
+import Skill from './components/Skill';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <main className='min-h-screen p-10 text-white'>
         <Hero />
         <Project />
+        <Skill />
+        <Contact />
       </main>
       <Footer />
     </div>
---
+++ my-app/src/components/Skill.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Skill = () => {
+  return (
+    <article className='bg-gray px-4 py-6 rounded-lg shadow-lg'>
+      <header className='text-2xl font-bold'>
+        <h1>Skills & Tools</h1>
+      </header>
+      <div className=' my-6'>
+        <p className='font-bold'>Dylan Barstow</p>
+        <p className='font-thin'>Saint Louis, Missouri</p>
+        <p className='border-b-2 w-3/6 mt-3 border-gray-300'>dcbarstow7@gmail.com</p>
+      </div>
+      <div className='flex flex-row gap-5'>
+        <img src='./images/github.svg' alt='github' />
+        <img src='./images/linkedin.svg' alt='linkedin' />
+        <img src='./images/codepen.svg' alt='codepen' />
+      </div>
+    </article>
+  );
+};
+
+export default Skill;
