db7a804437a53b204350d5dbbf9f83a8493132a6
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-07 16:48:27 -0500

    creating project and hero cards

---
+++ my-app/src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className='max-w-md h-96 flex flex-wrap content-center text-left'>
+      <h1 className='my-5 text-5xl font-bold'>Hello. My name is Dylan Barstow </h1>
+      <p>I'm a soon to be graduate of the Washington University 2U Full Stack Web Development program.</p>
+    </section>
+  );
+};
+
+export default Hero;
