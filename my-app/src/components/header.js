e4eaa76f0bccd9d79eb7a4c17366a3bb6967313c
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-07 20:01:30 -0500

    adjusted header structure

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='space-x-10'>
+    <nav className='space-x-10'>
       <ul className='flex flex-row space-x-4 md:space-x-10 md:text-xl justify-center md:justify-end font-thin'>
-        <li className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-          <a href='#projects'>Projects</a>
+        <li>
+          <a href='#projects' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+            Projects
+          </a>
         </li>
-        <li className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-          <a href='#contact'>Contact</a>
+        <li>
+          <a href='#contact' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+            Contact
+          </a>
         </li>
-        <li className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
-          <a href='#'>Resume</a>
-          <p>Resume</p>
+        <li>
+          <a href='#' target='_blank' rel='noreferrer' className='py-1 px-5 hover:bg-gray focus:bg-gray rounded-lg'>
+            Resume
+          </a>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 };
 
