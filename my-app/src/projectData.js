37b5b391515ebbd2a1015ce9c36f4b81b0d646a8
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-08 19:48:20 -0500

    importing imgs to projects

--- my-app/src/projectData.js
+++ my-app/src/projectData.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import Leaderboard from './images/Leaderboard.PNG'; // Tell webpack this JS file uses this image
+import fitnessTrackerHome from './images/fitnessTrackerHome.PNG';
+import NoteTakerHerokuDeployment2 from './images/NoteTakerHerokuDeployment2.PNG'
+
 const projects = [
   {
     title: 'The Iron Throne',
-    image: './images/#',
+    image: Leaderboard,
     description: 'The Iron Throne is a Game of Thrones personality quiz that pairs the user with a character from the series, your result will be shared with all other logged users to discover your allies.',
     tools: ['React', 'Redux', 'MongoDB', 'JWT', 'Express'],
     github: 'https://github.com/DylanBarstow7/theIronThrone',
@@ -9,7 +14,7 @@ const projects = [
   },
   {
     title: 'Note-Taker',
-    image: './images/#',
+    image: fitnessTrackerHome,
     description: 'A note taking app that uses an espress.js back-end and JSON to write, save, and delete notes.',
     tools: ['React', 'API'],
     github: 'https://github.com/DylanBarstow7/Note-Taker2',
@@ -17,7 +22,7 @@ const projects = [
   },
   {
     title: 'Fitness Tracker',
-    image: './images/#',
+    image: NoteTakerHerokuDeployment2,
     description: 'Chart your workout then go to your dashboard page to see charts containing the total weight and duration for the last 7 workouts. ',
     tools: ['Javascript', 'API', 'MongoDB', 'Express'],
     github: 'https://github.com/DylanBarstow7/Fitness-Tracker',
